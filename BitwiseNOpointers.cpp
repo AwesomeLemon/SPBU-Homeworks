bc0fc329e3cb2b3c5c1207721a8c564def795a5a
AwesomeLemon <a.e.chebykin@gmail.com>
2014-11-04 15:11:31 +0400

    The Very Final Version

--- BitwiseNOpointers.cpp
+++ BitwiseNOpointers.cpp
@@ -4,14 +4,16 @@
 
 
 #include <stdio.h>
-
+#include <stdlib.h>
+#define BIT_SIGN 31
+#define INT_START_NO_SIGN 30
 double apow(int a, int n) {
-    if (n >= 0) {
-    	int res = 1;
+    if (n >=  0) {
+    	int res  =  1;
     	while (n > 0) {
-    		if (n & 1) res *= a;
-    		a *= a;
-    		n = n >> 1;
+    		if (n & 1) res *=  a;
+    		a *=  a;
+    		n  =  n >> 1;
     	}
     	return (double) res;
     }
@@ -21,21 +23,23 @@ double apow(int a, int n) {
 	
 }
 
-void from10to2(int a) {
-	if (a) {
-		from10to2(a >> 1);
-		printf("%d",a & 1);
-	}
+void from10to2 (int a) {
+     if (!a) printf("0");
+     int k  =  INT_START_NO_SIGN;
+     while (!(a & 1 << k) && (k >= 0)) {
+           k--;
+     }
+     while (k >= 0) {
+           printf("%d",(a&1 << k) >> k--);
+     }
 }
-
-
 int fitsbits(int x, int n) {
-	int t=(1 << (n - 1));
+	int t = (1 << (n - 1));
 	return (!(x +  (~(((t + x) & ((1 << n) - 1)) + (~ t + 1)) + 1)   ));
 }
 
 int fsign(int x) {
-	return ((x >> 31) | (!! x));
+	return ((x >> BIT_SIGN) | (!! x));
 }
 int main() {
 	int res;
@@ -43,19 +47,19 @@ int main() {
 	/* //FSIGN
 	int a;
 	scanf("%d",&a);
-	res = fsign(a);
+	res  =  fsign(a);
 	printf("%d", res);*/
 
-	//FITSBITS
+	/* //FITSBITS
 	int x, n;
 	scanf("%d%d", &x, &n);
-	res = fitsbits(x, n);
+	res  =  fitsbits(x, n);
 	printf("%d", res);
-
+	*/
 	/* //APOW
 	int x, n;
 	scanf("%d%d",&x, &n);
-	double r = apow(x, n);
+	double r  =  apow(x, n);
 	printf("%llf", r);*/
 
 	/* //FROM10TO2
