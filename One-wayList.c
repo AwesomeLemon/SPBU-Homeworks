e40ff7ae72bc88efba8c1f1350b9051d81fa978c
AwesomeLemon <a.e.chebykin@gmail.com>
2014-11-11 09:39:33 +0400

    Mostly done
    Adding via switch doesn't work

--- One-wayList.c
+++ One-wayList.c
@@ -31,15 +31,61 @@ void printList(list* list1) {
 	printf("\n");
 }
 
-void pop(list* list1,float val) {
+int pop(list* list1,float val) {
 	node* curr = list1->head;
-	while ((curr->next != 0) && (curr->val != val)
+	if (curr->val == val) {
+            node* t = list1->head->next;
+            free(list1->head);
+            list1->head = t;   
+            return 0;  
+            }
+	while (curr->next != 0) {
+          if (curr->next->val != val)
+              curr = curr->next;
+          else {
+               node* t = curr->next->next;
+               free(curr->next);
+               curr->next = t;
+               return 0;
+               }
+          }
+ printf("Such element doesn't exist!");
+ return 1;
+}
 int main() {
 	list* list1 = (list*) malloc(sizeof(list));
-	list1->head = getNewnode(2.7);
-	list1->head->next=getNewnode(7.9);
+	//list1->head = getNewnode(2.7);
+	//list1->head->next=getNewnode(-7.9);
 	//getNewnod(2.3);
 	//getNewnod(-0.1);
-	printList(list1);
+//	printList(list1);
+//	pop(list1,2.7);
+//	printList(list1);
+    char c;
+    float val;
+    int flag=1;
+    node *curr = list1->head;
+    while (flag) {
+        scanf("%c",&c);
+        switch (c) {
+               case 'a':
+                    scanf("%f",&val);
+                    curr = getNewnode(val);
+                    curr = curr->next;
+                    printList(list1);
+                    break;
+               case 'r':
+                    scanf("%f",&val);
+                    pop(list1,val);
+                    printList(list1);
+                    break;
+               case 'p':
+                    printList(list1);
+                    break;
+               case 'q':
+                    flag = 0;
+                    }
+    }
+	system("pause");
 	return 0;
 }
