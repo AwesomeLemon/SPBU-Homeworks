5d6b48157af6ae5c726e9779c90b5375b77badb5
AwesomeLemon <a.e.chebykin@gmail.com>
2014-11-11 17:51:37 +0400

    Pre-Final Version (not checked)

--- One-wayListTest.c
+++ One-wayListTest.c
@@ -72,13 +72,24 @@ void push (node **head, float val) {
     tmp->next = (*head);
     (*head) = tmp;
 }
+void sendAlot(int n) {
+	freopen("in.txt","w+",stdout);
+	int i=0;
+	for (i; i<n; i++) printf("a %i.5\n",i);
+	printf("p q");
+	fclose(stdout);
+}
 int main() {
 	list* list1 = (list*) malloc(sizeof(list));
     char c;
     float val;
     int flag=1;
+	FILE* f;
 	list1->head = getnewNode(0);
 	pop(list1,0); //
+	sendAlot(750000);
+	freopen("in.txt","r",stdin);
+	freopen("out.txt","w+",stdout);
     while (flag) {
         scanf("%c",&c);
         switch (c) {
@@ -89,12 +100,16 @@ int main() {
                case 'r':
                     scanf("%f",&val);
                     pop(list1,val);
+					list1->len--;
                     break;
                case 'p':
+                    printList(list1);
                     break;
                case 'q':
                     flag = 0;
                     }
     }
+	fclose(stdin);
+	fclose(stdout);
 	return 0;
 }
