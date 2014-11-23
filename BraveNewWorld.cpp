d2bb39b7bd1d235b4a9b2e398b4e81c46320cfd6
AwesomeLemon <a.e.chebykin@gmail.com>
2014-11-23 19:35:49 +0400

    FindSubString homework
    NaiveSubString added. Everything else deleted.

--- BraveNewWorld.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <stdlib.h>
-using namespace std;
-int main() {
-	int n=7919;
-	cout << n << "We'll win";
-	system("pause");
-	return 0;
-}
--- HelloWorld.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <stdlib.h>
-using namespace std;
-int main() {
-	int n=10;
-	cout << "Hello World";
-	system("pause");
-	return 0;
-}
