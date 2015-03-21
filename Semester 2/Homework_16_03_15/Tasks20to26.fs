0d676cb8c7fb718825eae59f8f17a91aa7fc8ad8
AwesomeLemon <a.e.chebykin@gmail.com>
2015-03-21 11:28:51 +0400

    Homework from 16/03/15
    Contains tasks from 20 to 29

---
+++ Semester 2/Homework_16_03_15/Tasks20to26.fs
@@ -0,0 +1,155 @@
+﻿//Different graph interfaces and implementations
+//                by Alexander Chebykin
+open System
+//Task 20: graph interface
+type IGraph<'A>  =
+  interface
+    abstract IsWay : int -> int -> bool
+    abstract Size  : int
+    abstract ValAt : int -> 'A
+  end
+//Task 25: polymorph graph interface
+type IGraphMarked<'A>  =
+  interface
+    inherit IGraph<'A>
+    abstract WayLength  : int -> int -> int
+    abstract SetLengths : int[] -> unit
+  end
+//Task 21: IGraph implementation with adjacency matrix
+type ArrayOrgraph<'A> (verges: list<int*int>, numberOfNods, vals: 'A[]) =
+  class
+    let nodeVals = vals
+    let adjMatrix = Array2D.zeroCreate numberOfNods numberOfNods
+    do 
+      for (n1, n2) in verges do Array2D.set adjMatrix n1 n2 1
+    interface IGraph<'A> with
+      override s.IsWay a b =
+        if (adjMatrix.[a, b] = 1) then true else false
+      override s.Size = 
+        Array2D.length1 adjMatrix
+      override s.ValAt a = 
+        nodeVals.[a]
+    end
+  end
+//Task 21: IGraph implementation with adjacency lists
+type ListOrgraph<'A> (verges: list<int*int>, numberOfNods, vals: 'A[]) = 
+  class 
+    let nodeVals = vals
+    let nodes = Array.create numberOfNods []
+    do 
+      for (n1, n2) in verges do nodes.[n1] <- n2 :: nodes.[n1]
+    interface IGraph<'A> with
+      override s.IsWay a b =
+        let rec listFind x l = 
+          match l with
+          | [] -> None
+          | a :: l -> 
+            if (x = a) then Some x
+               else listFind x l
+        if (listFind b (nodes.[a]) = Some b) then true else false
+
+      override s.Size =
+        nodes.GetLength 0
+      override s.ValAt a = 
+        nodeVals.[a]
+    end
+  end
+//Task 23
+let canGoTo graph init = 
+  let visited = Array.create (graph :> IGraph<'A>).Size false
+  visited.[init] <- true
+  let rec find (visited :bool[]) gr cur = 
+    let mutable res = []
+    for i = 0 to ((gr :> IGraph<'A>).Size - 1) do
+      if ((gr :> IGraph<'A>).IsWay cur i) then 
+        if (visited.[i] = false) then 
+          visited.[i] <- true
+          res <- i :: res @ (find visited gr i)
+    res
+  List.sort (find visited graph init)
+//Task 24
+let canGetFrom graph init = 
+
+  let rec find (visited :bool[]) gr cur target = 
+    let mutable res = false
+    for i = 0 to ((gr :> IGraph<'A>).Size - 1) do
+      if ((gr :> IGraph<'A>).IsWay cur i) then 
+        if (visited.[i] = false) then 
+          if (i = target) then res <- true
+          else
+            visited.[i] <- true
+            res <- res || (find visited gr i target)
+    res
+
+  let mutable res = []
+  for i = 0 to ((graph :> IGraph<'A>).Size - 1) do
+    if not (i = init) then
+       let visited = Array.create (graph :> IGraph<'A>).Size false
+       visited.[i] <- true
+       if (find visited graph i init) then res <- i :: res
+  List.sort res
+
+type Computer(os)=
+  class
+    let name = os
+    let chanse =
+     match name with
+     | "Windows" -> 0.4
+     | "Linux" -> 0.1
+     | "OS X" -> 0.2
+     | _ -> 0.5
+    let mutable infection = 0 
+    member self.Print = 
+      printf "%A" infection
+    member self.InfectionChanse = chanse
+    member self.Infection = infection
+    member self.ChangeInfection value = //0 - healthy; 1 - has been infected on the current move (will become viral on the next move); 2 - viral
+      infection <- value
+  end
+
+type Net(verges: list<int*int>, numberOfNods, OSs :string[]) = 
+  class
+    inherit ArrayOrgraph<Computer> (verges, numberOfNods, [| for i in 0 .. OSs.Length - 1 -> new Computer(OSs.[i]) |])
+    member self.PrintInfection = 
+      for i = 0 to numberOfNods - 1 do
+        printf "%i " ((self  :> IGraph<Computer>).ValAt i).Infection
+      printfn ""
+  end
+
+let emulate (graph: Net) = 
+  let random = System.Random()
+  for i = 0 to (graph :> IGraph<Computer>).Size - 1 do
+    if ((graph :> IGraph<Computer>).ValAt i).Infection = 2 then 
+      for j = 0 to (graph :> IGraph<Computer>).Size - 1 do
+        if (((graph :> IGraph<Computer>).IsWay i j) && 
+            (((graph :> IGraph<Computer>).ValAt j).Infection = 0) &&
+              (random.NextDouble() < ((graph :> IGraph<Computer>).ValAt j).InfectionChanse)) 
+                then ((graph :> IGraph<Computer>).ValAt j).ChangeInfection 1
+  // And now infected Computers are getting completely viral so that on the next mobe they'll be able to infect their neighbours:
+  for i = 0 to (graph :> IGraph<Computer>).Size - 1 do
+   if ((graph :> IGraph<Computer>).ValAt i).Infection = 1 then ((graph :> IGraph<Computer>).ValAt i).ChangeInfection 2
+  graph 
+
+[<EntryPoint>]
+let main argv = 
+  let gr = new ArrayOrgraph<int> ([(0, 1); (1, 2); (2, 3); (2, 4); (3, 4); (1, 5); (1, 6); (5, 7); (4, 5)], 8, [|for i in 0..8 -> i|])
+  let gr2 = new ListOrgraph<int> ([(0, 1); (1, 2); (2, 3); (2, 4); (3, 4); (1, 5); (1, 6); (5, 7); (4, 5)], 8, [|for i in 0..8 -> i|])
+  //Task 23
+  let testNode = 3
+  printfn "You can get from node %i to nodes %A//Matrix implementation" testNode (canGoTo gr testNode)
+  printfn "You can get from node %i to nodes %A//Lists implementation" testNode (canGoTo gr2 testNode)
+  //Task 24
+  printfn "You can get to node %i from nodes %A//Matrix implementation" testNode (canGetFrom gr testNode)
+  printfn "You can get to node %i from nodes %A//Lists implementation" testNode (canGetFrom gr2 testNode)
+  //Task 26
+  printfn "Controls: press any key for the next move; press 'Esc' to quit.\nNotation: 2 - viral computer; 0 - healthy"
+  let mutable net = new Net ([(0, 1); (1, 2); (2, 3); (2, 4); (3, 4); (1, 5); (1, 6); (5, 7); (4, 5)], 8, [|"Windows"; "Linux"; "OS X"; "Linux"; "Unknown"; "Windows"; "Windows"; "OS X"|])
+  ((net :> IGraph<Computer>).ValAt 0).ChangeInfection 2 
+  let mutable flag = true
+  while flag do
+    match Console.ReadKey().Key with 
+    | ConsoleKey.Escape -> flag <- false
+    | _ -> 
+      net <- emulate net
+      net.PrintInfection
+  0 
---
+++ Semester 2/Homework_16_03_15/Tasks27to29.fs
@@ -0,0 +1,176 @@
+﻿//Interfaces and different implementatiuons of polymorphal list
+//            by Alexander Chebykin
+
+//Task 27
+type IMyList<'A when 'A: equality> = 
+  interface
+    abstract AddFront : 'A -> IMyList<'A>
+    abstract AddBack  : 'A -> IMyList<'A>
+    abstract AddAt    : int -> 'A -> IMyList<'A>
+    abstract DelFront : IMyList<'A>
+    abstract DelBack  : IMyList<'A>
+    abstract DelAt    : int -> IMyList<'A>
+    abstract Find     : 'A -> bool
+    abstract Concat   : IMyList<'A> -> IMyList<'A>
+  end
+
+type list<'A> = Nil | Cons of 'A * list<'A>
+
+type ATDList<'A when 'A : equality> (l) = 
+  class
+    let mutable list = l
+    let rec listPrint l = 
+      match l with
+      | Nil -> printf "\n"
+      | Cons (x, l) -> 
+      printf "%A " x
+      listPrint l
+    member self.Print = 
+      listPrint list
+    member self.Self = list
+    interface IMyList<'A> with
+      override self.AddFront value = 
+        ATDList<'A> (Cons (value, list)) :> IMyList<'A>
+
+      override self.AddBack value =
+        let rec addBack x list =
+          match list with
+          | Nil -> Cons (x, Nil)
+          | Cons (a, t) -> Cons (a, addBack x t)
+
+        ATDList<'A> (addBack value list) :> IMyList<'A>
+
+      override self.AddAt index value=
+        let rec addAt count x list = 
+          if count = 0 
+            then
+              match list with
+              | Cons (a, l) -> Cons (x, Cons (a, l))
+              | Nil -> Cons (x, Nil)
+            else
+              match list with
+              | Nil -> Cons (x, Nil)
+              | Cons (a, t) -> Cons (a, addAt (count - 1) x t)
+
+        ATDList<'A> (addAt (index - 1) value list) :> IMyList<'A>
+
+      override self.DelFront =
+        let a = match list with
+                | Nil -> Nil
+                | Cons (x, l) -> l
+
+        ATDList<'A> (a) :> IMyList<'A>
+
+      override self.DelBack = 
+        let rec delBack list =
+          match list with
+          | Nil -> Nil
+          | Cons (x, Nil) -> Nil
+          | Cons (x, l) -> Cons (x, delBack l)
+
+        ATDList<'A> (delBack list) :> IMyList<'A>
+      
+      override self.DelAt index =
+        let rec delAt count list = 
+          if count = 0 
+            then
+              match list with
+              | Cons (a, Cons (b, l)) -> Cons (b,l)
+              | Cons (a, Nil) -> failwith "Nothing to delete"
+              | Nil -> Nil
+            else
+              match list with
+              | Nil -> Nil
+              | Cons (a, t) -> Cons (a, delAt (count - 1) t)
+
+        ATDList<'A> (delAt (index - 1) list) :> IMyList<'A>
+
+      override self.Find value =
+        let rec find x list =
+          match list with
+          | Nil -> false
+          | Cons(a, t) ->
+            if (a = x) then true
+                       else find x t
+        find value list
+
+      override self.Concat (list2) = 
+        let rec concat list = 
+          match list with
+          | Nil -> (list2 :?> ATDList<'A>).Self
+          | Cons (a, l) -> Cons (a, concat l)
+        ATDList<'A> (concat list)  :> IMyList<'A>
+    end
+  end
+
+type ArrayList<'A when 'A : equality> (ar : 'A[], n) = 
+  class
+    let mutable array = ar
+    let last = n
+    let IncreaseNum = 9 // Number of elemnts by which we shall expand array at one time
+    member self.Print =
+      printfn "%A" array
+    member self.Self = array
+    member self.Last = last
+    interface IMyList<'A> with
+      override self.AddFront value =
+        ArrayList<'A> (Array.append [|value|] array, last + 1) :> IMyList<'A>
+      override self.AddBack value = 
+        ArrayList<'A> (Array.append array [|value|], last + 1) :> IMyList<'A>
+      override self.AddAt index value =
+        ArrayList<'A> (Array.append (Array.append array.[0..index-1] [|value|]) array.[index..array.Length-1], last + 1) :> IMyList<'A>
+      override self.DelFront =
+        ArrayList<'A> (array.[1..array.Length - 1], last - 1) :> IMyList<'A>
+      override self.DelBack =
+        ArrayList<'A> (array.[0..array.Length - 2], last - 1) :> IMyList<'A>
+      override self.DelAt index =
+        if (index < 0 || index > last) 
+          then failwith "There's no such element"
+          else
+            ArrayList<'A> (Array.append array.[0..index - 1] array.[index + 1..array.Length - 1], last - 1) :> IMyList<'A>
+      override self.Find value =
+        Array.exists (fun elem -> elem = value) array
+      override self.Concat list2 =
+        ArrayList<'A> (Array.append array ((list2 :?> ArrayList<'A>).Self), last + (list2 :?> ArrayList<'A>).Last + 1) :> IMyList<'A>
+  end
+[<EntryPoint>]
+let main argv =
+  //task 28
+  printfn "ATD implementation"
+  let list = new ATDList<int> (Cons (1, Cons (2, Cons (3, Nil))))
+  let list2 = new ATDList<int> (Cons (4, Cons (5, Cons (6, Nil))))
+  printfn "Initial list: "
+  list.Print
+  printfn "Adding 17 at the front, 12 to the 3rd(counting from 1) position, and 90 at the back: "
+  let list = ((((list :> IMyList<int>).AddFront 17)).AddBack 90).AddAt 3 12 :?> ATDList<int>
+  list.Print
+  printfn "Concatenating with list2 = [4; 5; 6]"
+  let list = ((list :> IMyList<int>).Concat list2) :?> ATDList<int>
+  list.Print
+  printfn "Deleting at the front and at the back"
+  let list = ((list :> IMyList<int>).DelFront).DelBack :?> ATDList<int>
+  list.Print
+  printfn "Deleting at 4th position (counting from 1)"
+  let list = (list :> IMyList<int>).DelAt 4 :?> ATDList<int>
+  list.Print
+  printfn "it is %A that 12 is in the list, and it is %A that 17 is in the list" ((list :> IMyList<int>).Find 12) ((list :> IMyList<int>).Find 17)
+  //task 29
+  printfn "Array implementation"
+  let list = new ArrayList<int> ([|1; 2; 3;|], 2)
+  let list2 = new ArrayList<int> ([|4; 5; 6;|], 2)
+  printfn "Initial list: "
+  list.Print
+  printfn "Adding 17 at the front, 12 to the 3rd(counting from 1) position, and 90 at the back: "
+  let list = ((((list :> IMyList<int>).AddFront 17)).AddBack 90).AddAt (3 - 1) 12 :?> ArrayList<int>
+  list.Print
+  printfn "Concatenating with list2 = [4; 5; 6]"
+  let list = ((list :> IMyList<int>).Concat list2) :?> ArrayList<int>
+  list.Print
+  printfn "Deleting at the front and at the back"
+  let list = ((list :> IMyList<int>).DelFront).DelBack :?> ArrayList<int>
+  list.Print
+  printfn "Deleting at 4th position (counting from 1)"
+  let list = (list :> IMyList<int>).DelAt (4 - 1)  :?> ArrayList<int>
+  list.Print
+  printfn "it is %A that 17 is in the list, and it is %A that 12 is in the list" ((list :> IMyList<int>).Find 17) ((list :> IMyList<int>).Find 12)
+  0
