83e67e0755d58e00c5bd9c2580865fbabfec6f0a
AwesomeLemon <a.e.chebykin@gmail.com>
2015-04-12 09:15:30 +0400

    Update AssemblyInfo.fs

--- Semester 2/Homework_06_04_15/InterpreterL/Tests/AssemblyInfo.fs
+++ Semester 2/Homework_06_04_15/InterpreterL/Tests/AssemblyInfo.fs
@@ -12,7 +12,4 @@ open System.Runtime.CompilerServices
 [<assembly:AssemblyTrademark("")>]
 [<assembly:AssemblyVersion("1.0.0.0")>]
 ()
-// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
-//[<assembly: AssemblyDelaySign(false)>]
-//[<assembly: AssemblyKeyFile("")>]
 
