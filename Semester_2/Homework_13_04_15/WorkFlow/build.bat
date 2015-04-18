739cebe676edd6d7f29bd257b100d2ec94d5c54c
AwesomeLemon <a.e.chebykin@gmail.com>
2015-04-18 19:57:43 +0400

    HW from 13/04/15 update
    'build.bat' updated
    'configure' added

--- Semester_2/Homework_13_04_15/WorkFlow/build.bat
+++ Semester_2/Homework_13_04_15/WorkFlow/build.bat
@@ -3,5 +3,4 @@
 cls
 
 ".nuget\NuGet.exe" "Install" "FAKE" "-OutputDirectory" "packages" "-ExcludeVersion"
-".nuget\NuGet.exe" "Install" "NUnit" "-OutputDirectory" "packages" "-ExcludeVersion"
 pause
