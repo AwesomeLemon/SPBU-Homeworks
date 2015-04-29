d4276d939b828d20b46e406da73627e373c52f9e
AwesomeLemon <a.e.chebykin@gmail.com>
2015-04-29 14:02:49 +0400

    Homework from 27/04/15
    Tasks 46 - 48

---
+++ Semester_2/Homework_27_04_15/Threads/Tests/MatrTests.fs
@@ -0,0 +1,14 @@
+﻿module MatrTests
+open Programs
+open NUnit.Framework
+
+[<TestCase (1, 10, TestName = "Matr: 1 thread, size 10")>]
+[<TestCase (1, 3000, TestName = "Matr: 1 thread, size 3000")>]
+[<TestCase (2, 3000, TestName = "Matr: 2 threads, size 3000")>]
+[<TestCase (4, 3000, TestName = "Matr: 4 threads, size 3000")>]
+[<TestCase (1000, 3000, TestName = "Matr: 100 threads, size 3000")>]
+let matrMulTest threadNumber size =
+  let a = Array2D.init 100 size (fun i j -> 2)
+  let b = Array2D.init size 100 (fun i j -> 3)
+  let res = matrMul threadNumber a b
+  Assert.AreEqual (res, Array2D.init 100 100 (fun i j-> 6 * size))
---
+++ Semester_2/Homework_27_04_15/Threads/Tests/maxTests.fs
@@ -0,0 +1,13 @@
+﻿module Tests
+open NUnit.Framework
+open Programs
+[<TestCase (1, 1000000, Result = 2147483082, TestName = "ArMax: 1 thread")>]
+[<TestCase (2, 1000000, Result = 2147483082, TestName = "ArMax: 2 threads")>]
+[<TestCase (3, 1000000, Result = 2147483082, TestName = "ArMax: 3 threads")>]
+[<TestCase (4, 1000000, Result = 2147483082, TestName = "ArMax: 4 threads")>]
+[<TestCase (33, 1000000, Result = 2147483082, TestName = "ArMax: 33 threads")>]
+let maxInArTest threadNumber arraySize : int =
+  let rnd = System.Random (0)
+  let arr = Array.init arraySize (fun _ -> rnd.Next())
+  let res = maxInAr threadNumber arr
+  res
